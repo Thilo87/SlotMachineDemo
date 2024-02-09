30a3d92a48c9b615e8fff5d5a1f68d2922ef5231
Thilo <nollthilo@hotmail.com>
2024-02-09 12:11:17 +0100

    Created BasicFunctionLibrary, DollarToText function

---
+++ Source/SlotMachineDemo/Private/BasicFunctionLibrary.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasicFunctionLibrary.h"
+
+#include "Kismet/KismetTextLibrary.h"
+
+void UBasicFunctionLibrary::DollarToText(float Dollar, FText& Text, bool bSwitchCentDollar)
+{
+	Text = FText();
+	
+	FString TextAsString;
+	
+	FString Sign = TEXT( "$" );
+	if ( FMath::Abs( Dollar ) < 1.f && bSwitchCentDollar )
+	{
+		Sign = TEXT( "¢" );
+		TextAsString = UKismetTextLibrary::Conv_FloatToText(
+			Dollar * 100.f,
+			HalfFromZero,
+			false,
+			true,
+			1,
+			324,
+			0,
+			0
+			).ToString();
+	}
+	else
+	{
+		TextAsString = UKismetTextLibrary::Conv_FloatToText(
+			Dollar,
+			HalfFromZero,
+			false,
+			true,
+			1,
+			324,
+			2,
+			2
+			).ToString();
+	}
+		
+
+	TextAsString += " " + Sign;
+	Text = FText::FromString( TextAsString );
+}
---
+++ Source/SlotMachineDemo/Public/BasicFunctionLibrary.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "BasicFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLOTMACHINEDEMO_API UBasicFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	/*
+	 *
+	 * Displaying text
+	 *
+	 */
+
+	UFUNCTION( BlueprintPure )
+	static void DollarToText( float Dollar, FText& Text, bool bSwitchCentDollar );
+};
