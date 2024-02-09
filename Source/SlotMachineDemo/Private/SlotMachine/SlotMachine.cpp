549c7cc8c7370da93651ddd4ae4fadeb1e22f420
Thilo <nollthilo@hotmail.com>
2024-02-09 20:25:45 +0100

    Rearranged folder structure

--- Source/SlotMachineDemo/Private/SlotMachine/SlotMachine.cpp
+++ Source/SlotMachineDemo/Private/SlotMachine/SlotMachine.cpp
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "SlotMachine.h"
+#include "SlotMachine/SlotMachine.h"
 
 #include "SlotMachineDemoGameMode.h"
 
--- Source/SlotMachineDemo/Private/SlotMachine/SlotMachineLine.cpp
+++ Source/SlotMachineDemo/Private/SlotMachine/SlotMachineLine.cpp
@@ -1,4 +1,4 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "SlotMachineElement.h"
+#include "SlotMachine/SlotMachineLine.h"
